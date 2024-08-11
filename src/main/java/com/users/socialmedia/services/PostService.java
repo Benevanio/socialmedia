ec21400ecff3374e5ed8b775e1f90b6e83890131
Bene-Tesla <benevaniosantos930@gmail.com>
2024-08-11 10:35:22 -0300

    feat: posts correlation

---
+++ src/main/java/com/users/socialmedia/services/PostService.java
@@ -0,0 +1,29 @@
+package com.users.socialmedia.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.users.socialmedia.domain.Post;
+import com.users.socialmedia.repository.PostRepository;
+import com.users.socialmedia.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+    @Autowired
+    private PostRepository postRepository;
+
+    public Post findById(String id) {
+        Post post = postRepository.findById(id).orElse(null);
+        if (post == null) {
+            throw new ObjectNotFoundException("Object not found");
+        }
+        return post;
+    }
+
+    public List<Post> findAll() {
+        return postRepository.findAll();
+    }
+
+}
